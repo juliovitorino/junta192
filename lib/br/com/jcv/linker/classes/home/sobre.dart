950ffbecbfa7d0bbeeeb283125efc9935a4c56fb
Julio <julio.vitorino.email@gmail.com>
2021-05-21 11:19:29 -0300

    inclusão de uma SSID no formulario

--- lib/br/com/jcv/linker/classes/home/sobre.dart
+++ lib/br/com/jcv/linker/classes/home/sobre.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:junta192/br/com/jcv/linker/classes/storages/cacheSession.dart';
 import 'package:junta192/br/com/jcv/linker/classes/storages/global_startup.dart';
 import 'package:junta192/br/com/jcv/linker/classes/ui/common/common-dataitem-title-text.dart';
 
 class Sobre extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String _token = CacheSession().getSession()['tokenid'];
     return Scaffold(
       appBar: AppBar(
         title: Text("Sobre"),
@@ -18,6 +20,7 @@ class Sobre extends StatelessWidget {
                   CommonDataItemTitleText("Ambiente Gateway Ativo", GlobalStartup().getAmbienteAtivo()),
                   CommonDataItemTitleText("Versão", GlobalStartup().getVersaoMin()),
                   CommonDataItemTitleText("Versão Build", GlobalStartup().getVersao()),
+                  CommonDataItemTitleText("SSID", _token.substring(0,4) + "******" + _token.substring(_token.length-4, _token.length)),
                   Platform.isIOS
                   ? CommonDataItemTitleText("Requer Dispositivo", "iPhone 7 ou superior")
                   : CommonDataItemTitleText("Requer Android", "6.0 (Marshmellow) ou superior"),
