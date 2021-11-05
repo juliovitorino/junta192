5be905b2d2d4d9357a47c1f8e9a8ba96e631848a
Julio <julio.vitorino.email@gmail.com>
2021-11-05 20:55:15 -0300

    geolocalização primaria de campanhas

--- lib/br/com/jcv/linker/classes/geolocalizacao/geolocalizacao-page.dart
+++ lib/br/com/jcv/linker/classes/geolocalizacao/geolocalizacao-page.dart
@@ -13,17 +13,55 @@ class GeoLocalizacaoPage extends StatefulWidget {
 }
 
 class _GeoLocalizacaoPageState extends State<GeoLocalizacaoPage> {
+  
+
+  Set<Marker> markers = Set<Marker>();
 
   @override
   Widget build(BuildContext context) {
+    markers.add(
+      Marker(
+        markerId: MarkerId("FAB J10-1"),
+        position: LatLng(-22.53446525795224, -44.06438902006575),
+      )
+    );
+
+    markers.add(
+      Marker(
+        markerId: MarkerId("FAB J10-2"),
+        position: LatLng(-22.53358329358749, -44.063530713206966),
+      )
+    );
+
+    markers.add(
+      Marker(
+        markerId: MarkerId("FAB J10-3"),
+        position: LatLng(-22.532228129499877, -44.063401967178145),
+      )
+    );
+
+
+    markers.add(
+      Marker(
+        markerId: MarkerId("FAB J10-4"),
+        position: LatLng(-22.53517627570643, -44.06424954521665),
+      )
+    );
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Onde tem Junta10"),
       ),
-      body: GoogleMap(initialCameraPosition: CameraPosition(
-        target: LatLng(-22.0,-44.12),
-        zoom: 18,
-      ),),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-22.5323031,-44.0635294),
+          zoom: 18.1521,
+        ),
+        mapType: MapType.normal,
+        myLocationEnabled: true,
+        markers: markers,
+      ),
     );
   }
 }
