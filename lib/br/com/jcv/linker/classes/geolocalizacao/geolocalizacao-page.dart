10a1979cd6931df8be99db067da72013612cff73
Julio <julio.vitorino.email@gmail.com>
2021-11-05 15:22:30 -0300

    Inicio da GeoLocalização do Junta10

---
+++ lib/br/com/jcv/linker/classes/geolocalizacao/geolocalizacao-page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GeoLocalizacaoPage extends StatefulWidget {
+  const GeoLocalizacaoPage({ Key key }) : super(key: key);
+
+  @override
+  _GeoLocalizacaoPageState createState() => _GeoLocalizacaoPageState();
+}
+
+class _GeoLocalizacaoPageState extends State<GeoLocalizacaoPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
